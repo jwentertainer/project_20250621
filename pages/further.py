471b9baafc9d4931eecda71af1e038ed98985dd7
jwentertainer <155875238+jwentertainer@users.noreply.github.com>
2025-06-22 16:42:24 +0900

    Update further.py

--- pages/further.py
+++ pages/further.py
@@ -9,6 +9,9 @@ csv_url = "https://raw.githubusercontent.com/jwentertainer/project_20250621/main
 # CSV 파일 읽기
 df = pd.read_csv(csv_url)
 
+# '시도'가 '서울'인 데이터만 필터링
+df_seoul = df[df['시도'] == '서울']
+
 # 표 출력
-st.subheader("데이터 미리보기")
-st.dataframe(df)
+st.subheader("서울 데이터 미리보기")
+st.dataframe(df_seoul)
